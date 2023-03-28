ebc56611d050c1e46e77d811bc28e8b9e3afa515
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2023-03-28 13:20:45 -0500

    Improved code and library

--- BasLem.py
+++ BasLem.py
@@ -1,92 +1,162 @@
 import re
 import unicodedata
 
-# example to test the performance
-text = '- Katua eta ni nire etxerantz joan ginen Bilboko kaleetatik jokatzen dut andereño.'
+class tokenization:
 
-# cleaning function
-def get_clean_text(data):
+    def __init__(self):
 
-    data = unicodedata.normalize('NFKD', data).encode('ascii', 'ignore').decode('utf-8')
+        try: #runs a try to make sure the requirements are fullfilled
+            import re
 
-    lowercase = data.lower()
-    numbers = re.sub('[0-9]', ' ', lowercase)
-    punctuation = re.sub('[^\w]', ' ', numbers)
-    spacing = re.sub("\s\s+", " ", punctuation)
+        except:
+            print("Regex library is not installed, try pip install re")
 
-    return spacing
+        try:
+            import unicodedata
 
-clean = get_clean_text(text)
+        except:
+            print("Unicodedata library is not installed, try pip install unicodedata")
 
-list_tokens = clean.split()
+    def get_clean_text(self, data):
 
-# lemmatizing and stemming
-def get_lemmas(data):
+        data = unicodedata.normalize('NFKD', data).encode('ascii', 'ignore').decode('utf-8')
 
-    changes_counter = 0
+        lowercase = data.lower()
+        numbers = re.sub('[0-9]', ' ', lowercase)
+        punctuation = re.sub('[^\w]', ' ', numbers)
+        spacing = re.sub("\s\s+", " ", punctuation)
+
+        return spacing
     
-    stoppers = ['eta', 'baina', 'zela']
-    eduki_verb = ['dut', 'duzu', 'du', 'dute', 'duzue', 'ditut', 'dituzu', 'ditu', 'dituzte', 'dituzue']
-    listatik = []
-    lemmatized_tokens = []
+    def get_lemmas(self, data):
 
-    for token in data:
-    
-        if token.endswith('a') and token not in stoppers:
-
-            token = token[:-1]
-            lemmatized_tokens.append(token)
-            changes_counter += 1
+        changes_counter = 0
         
-        elif token.endswith('rantz'):
-            token = token[:-5]
-            lemmatized_tokens.append(token)
-            changes_counter += 1
+        stoppers = ['eta', 'baina', 'zela']
+        eduki_verb = ['dut', 'duzu', 'du', 'dute', 'duzue', 'ditut', 'dituzu', 'ditu', 'dituzte', 'dituzue']
+        listatik = []
+        lemmatized_tokens = []
+        conjugations = ["a", 'rantz', 'ko', 'etatik', 'tatik', 'tzen']
+        conjugations = ['etatik', 'tatik', 'rantz', 'a' , 'k'] # check for biggest conjugations first
 
-        elif token.endswith('ko'):
-            token = token[:-2]
-            lemmatized_tokens.append(token)
-            changes_counter += 1
-
-        elif token.endswith('tatik'):
-
-            listatik.append(token)
-
-            for token in listatik:
-
-                if re.search(r'(etatik|tatik)$', token):
-
-                    if token.endswith('etatik'):
-
-                        token = token[:-6] #one character less to delete for double vowels
-                        lemmatized_tokens.append(token)
-                        changes_counter += 1
-
-                    else:
-                        token = token[:-5] #usual -tatik
-                        lemmatized_tokens.append(token)
-                        changes_counter += 1
-        
-        elif token.endswith('tzen'):
-
-            token = token[:-4]
-            token = token+str('tu')
-            lemmatized_tokens.append(token)
-            changes_counter += 1
-
-        elif token in eduki_verb:
-
-            token = 'eduki'
-            lemmatized_tokens.append(token)
-            changes_counter += 1
-
-        else:
-
-            lemmatized_tokens.append(token)
+        for token in data:
+            for conjugation in conjugations:
+                if token.endswith(conjugation) and token not in stoppers:
+                    token = token[:-1*len(conjugation)]
+                    lemmatized_tokens.append(token)
+                    changes_counter+=1
+                    break
+        return lemmatized_tokens
     
-    print(f'Changes made: {changes_counter}')
+    
 
-    return lemmatized_tokens
 
-lemmatized = get_lemmas(list_tokens)
-print(lemmatized)
+tokenizer = tokenization()
+
+text = "- Katua eta ni nire etxerantz joan ginen Bilboko kaleetatik jokatzen."
+
+data = tokenizer.get_clean_text(text)
+        
+
+
+# import re
+# import unicodedata
+
+# # example to test the performance
+# text = '- Katua eta ni nire etxerantz joan ginen Bilboko kaleetatik jokatzen.'
+
+# # cleaning function
+# def get_clean_text(data):
+
+#     data = unicodedata.normalize('NFKD', data).encode('ascii', 'ignore').decode('utf-8')
+
+#     lowercase = data.lower()
+#     numbers = re.sub('[0-9]', ' ', lowercase)
+#     punctuation = re.sub('[^\w]', ' ', numbers)
+#     spacing = re.sub("\s\s+", " ", punctuation)
+
+#     return spacing
+
+# clean = get_clean_text(text)
+
+# list_tokens = clean.split()
+
+# # lemmatizing and stemming
+# def get_lemmas(data):
+
+#     changes_counter = 0
+    
+#     stoppers = ['eta', 'baina', 'zela']
+#     eduki_verb = ['dut', 'duzu', 'du', 'dute', 'duzue', 'ditut', 'dituzu', 'ditu', 'dituzte', 'dituzue']
+#     listatik = []
+#     lemmatized_tokens = []
+#     conjugations = ["a", 'rantz', 'ko', 'etatik', 'tatik', 'tzen']
+#     conjugations = ['etatik', 'tatik', 'rantz', 'a' , 'k'] # check for biggest conjugations first
+
+#     for token in data:
+#         for conjugation in conjugations:
+#             if token.endswith(conjugation) and token not in stoppers:
+#                 token = token[:-1*len(conjugation)]
+#                 lemmatized_tokens.append(token)
+#                 changes_counter+=1
+#                 break
+
+    
+#         if token.endswith('a') and token not in stoppers:
+
+#             token = token[:-1]
+#             lemmatized_tokens.append(token)
+#             changes_counter += 1
+        
+#         elif token.endswith('rantz'):
+#             token = token[:-5]
+#             lemmatized_tokens.append(token)
+#             changes_counter += 1
+
+#         elif token.endswith('ko'):
+#             token = token[:-2]
+#             lemmatized_tokens.append(token)
+#             changes_counter += 1
+
+#         elif token.endswith('tatik'):
+
+#             listatik.append(token)
+
+#             for token in listatik:
+
+#                 if re.search(r'(etatik|tatik)$', token):
+
+#                     if token.endswith('etatik'):
+
+#                         token = token[:-6] #one character less to delete for double vowels
+#                         lemmatized_tokens.append(token)
+#                         changes_counter += 1
+
+#                     else:
+#                         token = token[:-5] #usual -tatik
+#                         lemmatized_tokens.append(token)
+#                         changes_counter += 1
+        
+#         elif token.endswith('tzen'):
+
+#             token = token[:-4]
+#             token = token+str('tu')
+#             lemmatized_tokens.append(token)
+#             changes_counter += 1
+
+#         elif token in eduki_verb:
+
+#             token = 'eduki'
+#             lemmatized_tokens.append(token)
+#             changes_counter += 1
+
+#         else:
+
+#             lemmatized_tokens.append(token)
+    
+#     print(f'Changes made: {changes_counter}')
+
+#     return lemmatized_tokens
+
+# lemmatized = get_lemmas(list_tokens)
+# print(lemmatized)
