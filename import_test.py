091b341fc5d867e523468a65a1b3bdd3b6414589
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2023-03-29 14:39:47 -0500

    updates eduki

--- import_test.py
+++ import_test.py
@@ -1,10 +1,13 @@
+# Import library 
 import BasLem
 
+# Set tokenizer 
 tokenizer = BasLem.Tokenization()
 
+# Import text to be lemmatized/stemmed 
 text = "- Katua eta ni nire etxerantz joan ginen Bilboko kaleetatik jokatzen zituzten."
 
+# Process the text
 data = tokenizer.get_clean_text(text)
 lemmas = tokenizer.get_lemmas(data)
-
 print(lemmas)
